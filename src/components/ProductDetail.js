efdabd0b059786227f7bd85c23a8fb81306ae610
GoatFeatherFarm <44263291+missybarringer@users.noreply.github.com>
2019-08-23 18:55:51 -0500

    adding buy now button

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,8 +1,20 @@
-import React, { useState } from "react"
+import React, { useState, useLayoutEffect } from "react"
 import Img from "gatsby-image"
+import ShopifyBuy from '@shopify/buy-button-js'
 
 const ProductDetail = ({ product }) => {
   const [selectedVariant, setVariant] = useState(product.variants[0])
+
+  useLayoutEffect(() => {
+    
+    const client = ShopifyBuy.buildClient({
+      domain: 'web-wabi-sabi.store.myshopify.com',
+      storefrontAccessToken: '50c676159b0e37611699ecd324eae2c2'
+    });
+    const ui = ShopifyBuy.UI.init(client);
+    console.log('ui', ui);
+    return () => {}, []
+  })
 
   return (
     <div>
@@ -10,6 +22,7 @@ const ProductDetail = ({ product }) => {
       <Img fixed={product.images[0].localFile.childImageSharp.fixed} />
       <p>{product.description}</p>
       <p>${selectedVariant.price}</p>
+      <div id="button"></div>
       <select
         onChange={e => {
           const selected = product.variants.filter(
